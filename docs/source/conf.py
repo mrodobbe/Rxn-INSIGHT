cf24a08610887feec3dbdcc990b0beb56be3333a
mrodobbe <mrodobbe.Dobbelaere@UGent.be>
2024-12-05 20:30:46 +0100

    update theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,6 +25,7 @@ extensions = [
     "sphinx.ext.mathjax",
     "sphinx.ext.viewcode",
     "sphinx.ext.doctest",
+    "sphinx_rtd_theme"
 ]
 
 bibtex_bibfiles = ['refs.bib']
@@ -36,5 +37,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
