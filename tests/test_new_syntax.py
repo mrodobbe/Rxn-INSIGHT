8d6f24e40e393796764713aecf9fd63607cb4563
mrodobbe <mrodobbe.Dobbelaere@UGent.be>
2025-03-19 13:00:35 +0100

    add new tests for Molecule

--- tests/test_new_syntax.py
+++ tests/test_new_syntax.py
@@ -6,4 +6,11 @@ def test_load():
 
 
 def test_molecule():
-    m = ri.Molecule("CCCO")
+    m = ri.Molecule("c1ccccc1", allow_pubchem=True)
+    assert m.iupac_name.lower() == "benzene"
+
+
+def test_molecule_rings():
+    m = ri.Molecule("Cc1ccccc1", allow_pubchem=False)
+    rings = m.get_rings()
+    assert rings == ["c1ccccc1"]
